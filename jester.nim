ce67dd413eb167aec9116c2b76472cb95aa66b11
Dominik Picheta <dominikpicheta@googlemail.com>
2012-12-02 13:17:05 +0000

    Changed folder structure for babel.

--- jester.nim
+++ jester.nim
@@ -3,7 +3,9 @@
 import httpserver, sockets, strtabs, re, tables, parseutils, os, strutils, uri,
         scgi, cookies, times, mimetypes, asyncio
 
-import patterns, errorpages, utils
+import jester/private/patterns, 
+       jester/private/errorpages,
+       jester/private/utils
 
 from cgi import decodeData, ECgi
 
