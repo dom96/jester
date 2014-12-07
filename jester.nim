de5ea45fc1d420d6ef1c002f8cb6417605bf6410
Dominik Picheta <dominikpicheta@googlemail.com>
2014-12-07 17:50:33 +0000

    Some cleanup.

--- jester.nim
+++ jester.nim
@@ -743,7 +743,7 @@ macro routes*(body: stmt): stmt {.immediate.} =
   result.add(matchProc)
 
   result.add parseExpr("jester.serve(match, settings)")
-  echo toStrLit(result)
+  #echo toStrLit(result)
   #echo treeRepr(result)
 
   
