e42c7493b0d0e114df19195e8260f6bc1f819ebd
Dominik Picheta <dominikpicheta@gmail.com>
2017-10-20 18:22:31 +0100

    Fixes tester on 0.17.2.

--- tests/tester.nim
+++ tests/tester.nim
@@ -4,24 +4,24 @@ import unittest, httpclient, strutils, asyncdispatch
 
 const port = 5454
 
-var client = newAsyncHttpClient()
+var client = newAsyncHttpClient(maxRedirects = 0)
 
 test "can access root":
   # If this fails then alltest is likely not running.
   let resp = waitFor client.get("http://localhost:" & $port & "/foo/")
   check resp.status.startsWith("200")
-  check resp.body == "Hello World"
+  check (waitFor resp.body) == "Hello World"
 
 test "/halt":
   let resp = waitFor client.get("http://localhost:" & $port & "/foo/halt")
   check resp.status.startsWith("502")
-  check resp.body == "I'm sorry, this page has been halted."
+  check (waitFor resp.body) == "I'm sorry, this page has been halted."
 
 test "/guess":
   let resp = waitFor client.get("http://localhost:" & $port & "/foo/guess/foo")
-  check resp.body == "Haha. You will never find me!"
+  check (waitFor resp.body) == "Haha. You will never find me!"
   let resp2 = waitFor client.get("http://localhost:" & $port & "/foo/guess/Frank")
-  check resp2.body == "You've found me!"
+  check (waitFor resp2.body) == "You've found me!"
 
 test "/redirect":
   let resp = waitFor client.request("http://localhost:" & $port & "/foo/redirect/halt", httpGet)
@@ -29,8 +29,8 @@ test "/redirect":
 
 test "regex":
   let resp = waitFor client.get("http://localhost:" & $port & "/foo/02.html")
-  check resp.body == "02"
+  check (waitFor resp.body) == "02"
 
 test "resp":
   let resp = waitFor client.get("http://localhost:" & $port & "/foo/resp")
-  check resp.body == "This should be the response"
+  check (waitFor resp.body) == "This should be the response"
